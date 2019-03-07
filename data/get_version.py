59945746febd49ac1f895858d238b0d361a094bd
Chris Griffith <chris@cdgriffith.com>
2019-03-06 23:14:53 -0600

    Update version to use datetime because of github tagging

--- data/get_version.py
+++ data/get_version.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+from datetime import datetime as dt
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 from flix.version import __version__
@@ -17,14 +18,14 @@ build_id = os.getenv('APPVEYOR_BUILD_ID')
 branch = os.getenv('APPVEYOR_REPO_BRANCH', 'none')
 pr_branch = os.getenv('APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH')
 pr_number = os.getenv('APPVEYOR_PULL_REQUEST_NUMBER')
-commit = os.getenv('APPVEYOR_REPO_COMMIT', '0000000')[:7]
+now = dt.now().strftime('%Y.%m.%d-%H.%M')
 
 if not pr_branch and branch == 'master':
     write_and_exit(__version__)
 
 elif pr_branch:
-    write_and_exit(f'{__version__}-pr-{pr_number}')
+    write_and_exit(f'{__version__}-pr-{pr_number}-{now}')
 
 else:
-    write_and_exit(f'{__version__}-{branch}-{commit}')
+    write_and_exit(f'{__version__}-{branch}-{now}')
 
