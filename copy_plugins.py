c8bc5dee224c0621fd791a468c975e3c688fdf4e
Chris Griffith <chris@cdgriffith.com>
2019-05-04 09:59:48 -0500

    Updating copy plugins file for other devs

--- copy_plugins.py
+++ copy_plugins.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import shutil
+import os
+from appdirs import user_data_dir
+from pathlib import Path
 
 from flix.version import __version__
 
-dest = f'C:\\Users\\teckc\\AppData\\Roaming\\FastFlix\\{__version__}\\plugins'
+data_path = Path(user_data_dir("FastFlix", appauthor=False, version=__version__, roaming=True))
+data_path.mkdir(parents=True, exist_ok=True)
+
+
+dest = f'{data_path}{os.sep}plugins'
+
+print(f"Copying plugins to {dest}")
 
 shutil.rmtree(dest, ignore_errors=True)
-shutil.copytree('flix\\plugins', dest)
+shutil.copytree(f'flix{os.sep}plugins', dest)
