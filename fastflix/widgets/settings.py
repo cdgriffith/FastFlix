cbe469eec73dc62330fc78a160466dafb5a2a775
Chris Griffith <chris@cdgriffith.com>
2020-08-26 19:36:02 -0500

    start

---
+++ fastflix/widgets/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtWidgets, QtCore, QtGui
+
+
+class AV1(QtWidgets.QWidget):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
