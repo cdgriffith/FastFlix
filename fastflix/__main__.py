b76baca9f2ad196c2bf3bbad8a5cd100a2caffc4
Chris Griffith <chris@cdgriffith.com>
2020-10-23 19:02:39 -0500

    * Fixing #92 cannot start FastFlix on Mac (thanks to Paul Bakaus)

--- fastflix/__main__.py
+++ fastflix/__main__.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+import traceback
+from multiprocessing import freeze_support
+
 from fastflix.gui import main
 
-main()
+
+def start_fastflix():
+    freeze_support()
+    try:
+        main()
+    except Exception:
+        traceback.print_exc()
+        input(
+            "Error while running FastFlix!\n"
+            "Plese report this issue on https://github.com/cdgriffith/FastFlix/issues (press any key to exit)"
+        )
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    start_fastflix()
