958cd5135656ec50100de4ac4ca1f4237def4eea
Chris Griffith <chris@cdgriffith.com>
2020-12-05 16:47:28 -0600

    Try adding tests for mac

--- fastflix/entry.py
+++ fastflix/entry.py
@@ -41,7 +41,77 @@ def separate_app_process(worker_queue, status_queue, log_queue):
     start_app(worker_queue, status_queue, log_queue)
 
 
+def startup_options():
+    options = sys.argv[1:]
+
+    if "--test" in options:
+        try:
+            import appdirs
+            import colorama
+            import coloredlogs
+            import iso639
+            import mistune
+            import box
+            import qtpy
+            import requests
+            import reusables
+            import ruamel.yaml
+
+            import fastflix.flix
+            import fastflix.language
+            import fastflix.program_downloads
+            import fastflix.resources
+            import fastflix.shared
+            import fastflix.version
+            import fastflix.models.config
+            import fastflix.models.fastflix
+            import fastflix.models.fastflix_app
+            import fastflix.models.base
+            import fastflix.models.encode
+            import fastflix.models.video
+            import fastflix.widgets.container
+            import fastflix.widgets.progress_bar
+            import fastflix.widgets.about
+            import fastflix.widgets.changes
+            import fastflix.widgets.logs
+            import fastflix.widgets.main
+            import fastflix.widgets.profile_window
+            import fastflix.widgets.settings
+            import fastflix.widgets.thumbnail_generator
+            import fastflix.widgets.video_options
+            import fastflix.widgets.panels.abstract_list
+            import fastflix.widgets.panels.audio_panel
+            import fastflix.widgets.panels.cover_panel
+            import fastflix.widgets.panels.command_panel
+            import fastflix.widgets.panels.queue_panel
+            import fastflix.widgets.panels.status_panel
+            import fastflix.widgets.panels.subtitle_panel
+            import fastflix.encoders.av1_aom.main
+            import fastflix.encoders.avc_x264.main
+            import fastflix.encoders.copy.main
+            import fastflix.encoders.gif.main
+            import fastflix.encoders.hevc_x265.main
+            import fastflix.encoders.rav1e.main
+            import fastflix.encoders.svt_av1.main
+            import fastflix.encoders.vp9.main
+            import fastflix.encoders.webp.main
+            import fastflix.encoders.common.attachments
+            import fastflix.encoders.common.audio
+            import fastflix.encoders.common.helpers
+            import fastflix.encoders.common.setting_panel
+            import fastflix.encoders.common.subtitles
+        except Exception as err:
+            print(f"Error: {err}")
+            sys.exit(1)
+        print("Success")
+        sys.exit(0)
+    if "--version" in options:
+        print(__version__)
+        sys.exit(0)
+
+
 def main():
+    startup_options()
     logger = logging.getLogger("fastflix-core")
     logger.addHandler(reusables.get_stream_handler(level=logging.DEBUG))
     logger.setLevel(logging.DEBUG)
