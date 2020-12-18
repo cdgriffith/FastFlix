2b892c946104e1c9a9a314c2b117ed6fdf2e8a40
Chris Griffith <chris@cdgriffith.com>
2020-12-18 17:30:57 -0600

    * Fixing dispositions being set for wrong tracks (thanks to ftpmorph)

--- fastflix/encoders/common/subtitles.py
+++ fastflix/encoders/common/subtitles.py
@@ -12,7 +12,9 @@ def build_subtitle(subtitle_tracks, subtitle_file_index=0):
             outdex = track.outdex - (1 if burn_in_track else 0)
             command_list.append(f"-map {subtitle_file_index}:{track.index} -c:{outdex} copy ")
             if track.disposition:
-                command_list.append(f"-disposition:s:{outdex} '{track.disposition}'")
+                command_list.append(f"-disposition:{outdex} '{track.disposition}'")
+            else:
+                command_list.append(f"-disposition:{outdex} 0")
             command_list.append(f"-metadata:s:{outdex} language='{track.language}'")
 
     return " ".join(command_list), burn_in_track
--- fastflix/version.py
+++ fastflix/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__version__ = "4.0.0b3"
+__version__ = "4.0.0b4"
 __author__ = "Chris Griffith"
