fa117da090f24809b515927bf24f74ea156a5562
Etz <edsuls@gmail.com>
2021-03-09 20:39:36 +0200

    Add nlmeans_opencl support
    Add nlmeans_opencl support

--- fastflix/widgets/panels/advanced_panel.py
+++ fastflix/widgets/panels/advanced_panel.py
@@ -38,6 +38,11 @@ denoise_presets = {
         "moderate": "nlmeans=s=1.0:p=7:r=15",
         "strong": "nlmeans=s=10.0:p=13:r=25",
     },
+    "nlmeans_opencl": {
+        "weak": "nlmeans=s=1.0:p=3:r=9",
+        "moderate": "nlmeans=s=1.0:p=7:r=15",
+        "strong": "nlmeans=s=10.0:p=13:r=25",
+    },
     "atadenoise": {
         "weak": "atadenoise=0a=0.01:0b=0.02:1a=0.01:1b=0.02:2a=0.01:2b=0.02:s=9",
         "moderate": "atadenoise=0a=0.02:0b=0.04:1a=0.02:1b=0.04:2a=0.02:2b=0.04:s=9",
