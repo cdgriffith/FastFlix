f401770252005bc1f7570098f95ad246b8e95c8a
Chris Griffith <chris@cdgriffith.com>
2023-03-18 13:21:35 -0500

    Adding parsers for rigaya's encoders

--- fastflix/rigaya_helpers.py
+++ fastflix/rigaya_helpers.py
@@ -1,37 +1,81 @@
 # -*- coding: utf-8 -*-
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from subprocess import run, PIPE
 
 
 @dataclass
-class VCEEncoder:
-    device_number: int
-    device_name: str
-    encoders: list[str]
+class Encoder:
+    name = "UNSET"
+    device_number: int = 0
+    device_name: str = ""
+    formats: list[str] = field(default_factory=list)
+
+
+class VCEEncoder(Encoder):
+    name = "VCE"
+
+
+class NVENCEncoder(Encoder):
+    name = "NVENC"
+
+
+class QSVEncoder(Encoder):
+    name = "QSV"
 
 
 def parse_vce_devices(split_text: list[str]):
-    device_number = 0
-    device_name = ""
-    encoders = []
+    encoder = VCEEncoder()
 
     for line in split_text:
         line = line.strip()
         if line.startswith("device #"):
             start, end = line.split("#", 1)
             device_number, device_name = end.split(":", 1)
+            encoder.device_name = device_name.strip()
+            encoder.device_number = int(device_number)
+
         if line.endswith("encode features"):
-            encoders.append(line.split()[0])
+            encoder.formats.append(line.split()[0].strip())
+    return encoder
 
-    return VCEEncoder(device_number=int(device_number), device_name=device_name.strip(), encoders=encoders)
+
+def parse_nvenc_devices(split_text: list[str]):
+    encoder = NVENCEncoder()
+
+    for line in split_text:
+        line = line.strip()
+        if line.startswith("#"):
+            device_number, device_name = line.split(":", 1)
+            encoder.device_name = device_name.split("(", 1)[0].strip()
+            encoder.device_number = int(device_number[1:])
+        if line.startswith("Codec:"):
+            encoder.formats.append(line.split(":", 1)[1].strip())
+    return encoder
 
 
-def run_check_features(executable):
+def parse_qsv_devices(split_text: list[str]):
+    encoder = QSVEncoder()
+
+    for line in split_text:
+        line = line.strip()
+        if line.startswith("GPU"):
+            device_name = line.split(":", 1)[1]
+            encoder.device_name = device_name.split("(", 1)[0].strip()
+        if line.startswith("Codec:"):
+            encoder.formats.append(line.split(":", 1)[1].strip())
+    return encoder
+
+
+def run_check_features(executable, encoder="UNSET"):
     outputs = []
-    for i in range(10):
-        result = run([executable, "--check-features", str(i)], stdout=PIPE, stderr=PIPE, encoding="utf-8")
-        if result.stderr:
-            break
+    if encoder == "qsv":
+        result = run([executable, "--check-features"], stdout=PIPE, stderr=PIPE, encoding="utf-8")
         outputs.append(result.stdout.splitlines())
+    else:
+        for i in range(10):
+            result = run([executable, "--check-features", str(i)], stdout=PIPE, stderr=PIPE, encoding="utf-8")
+            if result.stderr:
+                break
+            outputs.append(result.stdout.splitlines())
 
     return outputs
--- tests/test_flix.py
+++ tests/test_flix.py
@@ -1,73 +1,56 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
-from box import Box
+from fastflix.rigaya_helpers import (
+    parse_vce_devices,
+    VCEEncoder,
+    parse_nvenc_devices,
+    NVENCEncoder,
+    QSVEncoder,
+    parse_qsv_devices,
+)
 
-from fastflix.models.config import Config
+here = Path(__file__).parent
+assets = here / "assets"
 
-from fastflix.rigaya_helpers import parse_vce_devices, VCEEncoder
-
-fake_app = Box(default_box=True)
-fake_app.fastflix.config = Config()
+test_logs = {
+    "vce": [
+        {
+            "text": (assets / "rigaya_vce_cf_5800h_win10.txt").read_text(encoding="utf-8").splitlines(),
+            "result": VCEEncoder(device_number=0, device_name="AMD Radeon", formats=["H.264/AVC", "H.265/HEVC"]),
+        }
+    ],
+    "nvenc": [
+        {
+            "text": (assets / "rigaya_nvenc_cf_3060_linux.txt").read_text(encoding="utf-8").splitlines(),
+            "result": NVENCEncoder(
+                device_number=0, device_name="NVIDIA GeForce RTX 3060", formats=["H.264/AVC", "H.265/HEVC"]
+            ),
+        }
+    ],
+    "qsv": [
+        {
+            "text": (assets / "rigaya_qsv_cf_i5_9400_win10.txt").read_text(encoding="utf-8").splitlines(),
+            "result": QSVEncoder(
+                device_number=0,
+                device_name="Intel UHD Graphics 630",
+                formats=["H.264/AVC PG", "H.264/AVC FF", "H.265/HEVC PG", "MPEG2 PG", "MPEG2 FF"],
+            ),
+        }
+    ],
+}
 
 
 def test_parse_vce():
-    example_text = """
-device #0: AMD Radeon
-H.264/AVC encode features
-10bit depth:     no
-acceleration:    Hardware-accelerated
-max profile:     High
-max level:       unknown
-max bitrate:     2147483 kbps
-ref frames:      1-16
-Bframe support:  no
-HW instances:    1
-max streams:     16
-timeout support: yes
+    for vce_test in test_logs["vce"]:
+        assert parse_vce_devices(vce_test["text"]) == vce_test["result"]
 
-H.264/AVC input:
-Width:       128 - 4096
-Height:      128 - 4096
-alignment:   32
-Interlace:   no
-pix format:  NV12[1](native), YUV420P[7], YV12[2], BGRA[3], RGBA[5], ARGB[4]
-memory type: DX11(native), OPENCL, OPENGL, HOST
 
-H.264/AVC output:
-Width:       128 - 4096
-Height:      128 - 4096
-alignment:   32
-Interlace:   no
-pix format:  UNKNOWN[0](native)
-memory type: DX11(native), OPENCL, OPENGL, HOST
+def test_nvenc_parse():
+    for nvenc_test in test_logs["nvenc"]:
+        assert parse_nvenc_devices(nvenc_test["text"]) == nvenc_test["result"]
 
-H.265/HEVC encode features
-10bit depth:     yes
-acceleration:    Hardware-accelerated
-max profile:     main
-max level:       unknown
-max bitrate:     2147483 kbps
-ref frames:      1-16
-max streams:     16
-timeout support: yes
 
-H.265/HEVC input:
-Width:       128 - 4096
-Height:      128 - 4096
-alignment:   32
-Interlace:   no
-pix format:  NV12[1](native), YUV420P[7], YV12[2], BGRA[3], RGBA[5], ARGB[4]
-memory type: DX11(native), OPENCL, OPENGL, HOST
-
-H.265/HEVC output:
-Width:       128 - 4096
-Height:      128 - 4096
-alignment:   32
-Interlace:   no
-pix format:  UNKNOWN[0](native)
-memory type: DX11(native), OPENCL, OPENGL, HOST"""
-
-    assert parse_vce_devices(example_text.splitlines()) == VCEEncoder(
-        device_number=0, device_name="AMD Radeon", encoders=["H.264/AVC", "H.265/HEVC"]
-    )
+def test_qsv_parse():
+    for qsv_test in test_logs["qsv"]:
+        assert parse_qsv_devices(qsv_test["text"]) == qsv_test["result"]
