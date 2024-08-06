20ec4f5759f9ef06f9cd444fe0747bdd609d5329
Chris Griffith <chris@cdgriffith.com>
2024-08-05 20:22:41 -0500

    * Fixing #586 audio channels being set incorrectly (thanks to Hankuu) * Fixing #588 audio and subtitle dispositions were not set from source (thanks to GeZorTenPlotZ)

--- fastflix/version.py
+++ fastflix/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__version__ = "5.7.4"
+__version__ = "5.7.5"
 __author__ = "Chris Griffith"
--- fastflix/widgets/panels/audio_panel.py
+++ fastflix/widgets/panels/audio_panel.py
@@ -202,6 +202,7 @@ class Audio(QtWidgets.QTabWidget):
         self.app.fastflix.current_video.audio_tracks[self.index].language = self.language
         if not self.loading:
             self.check_conversion_button()
+            self.check_dis_button()
             return self.parent.main.page_update(build_thumbnail=False)
 
     @property
@@ -285,6 +286,13 @@ class Audio(QtWidgets.QTabWidget):
         else:
             self.widgets.conversion.setStyleSheet("")
             self.widgets.conversion.setText(t("Conversion"))
+
+    def check_dis_button(self):
+        audio_track: AudioTrack = self.app.fastflix.current_video.audio_tracks[self.index]
+        if any(audio_track.dispositions.values()):
+            self.widgets.disposition.setStyleSheet("border-color: #0055ff")
+        else:
+            self.widgets.disposition.setStyleSheet("")
 
 
 class AudioList(FlixList):
