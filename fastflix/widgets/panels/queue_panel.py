f451d21d2848ca70fe1cbf11c1f6abc24a36a03a
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-11-26 00:27:51 +0100

    Fix process priority (#708)

--- fastflix/widgets/panels/queue_panel.py
+++ fastflix/widgets/panels/queue_panel.py
@@ -230,8 +230,11 @@ class EncodingQueue(FlixList):
         self.load_queue_button.setFixedWidth(110)
 
         self.priority_widget = QtWidgets.QComboBox()
-        self.priority_widget.addItems(["Realtime", "High", "Above Normal", "Normal", "Below Normal", "Idle"])
-        self.priority_widget.setCurrentIndex(3)
+        self.priority_widget.addItems(
+            ([] if reusables.win_based else ["Realtime"])
+            + ["High", "Above Normal", "Normal", "Below Normal", "Idle"]
+        )
+        self.priority_widget.setCurrentText("Normal")
         self.priority_widget.currentIndexChanged.connect(self.set_priority)
 
         self.clear_queue = QtWidgets.QPushButton(
